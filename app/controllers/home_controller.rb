ec9a8daf491c23970b6b03bb68d748eb4ad4d4c8
praveen-agileblaze <praveenm@agileblaze.com>
2013-09-06 14:19:19 +0530

    initial commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,47 @@
+require 'net/http'
+
+class HomeController < ApplicationController
+
+  def get_detail
+    	location = params[:location]
+      page = params[:page]
+    	location = location.gsub(" ","_")
+
+    	url = "/wiki/en/api.php?action=query&prop=revisions&rvprop=content&rvsection=#{page}&titles=#{location}&format=xml&rvparse"
+      response = Net::HTTP.get_response("wikitravel.org", url)
+    	data = Hash.from_xml(response.body)
+
+      main_html = data["api"]["query"]["pages"]["page"]["revisions"]["rev"]
+      if main_html.include?("</h3>\n")
+          main_html = main_html.split("</h3>\n")[1]
+      elsif main_html.include?("</h2>\n")
+          main_html = main_html.split("</h2>\n")[1]
+      elsif main_html.include?("</h1>\n")    
+          main_html = main_html.split("</h1>\n")[1]
+      end
+
+      main_html = main_html.gsub("\"/", "\"http://wikitravel.org/")
+      main_html = main_html.gsub("href=\"h", "target='_blank' href=\"h")
+
+      render :json => {:content => main_html}
+  end
+
+  def result
+      @lines = []
+      unless params[:location].nil? || params[:location] == ""
+          location = params[:location]
+          location = location.gsub(" ","_")
+          url = "/wiki/en/api.php?format=xml&action=parse&prop=sections&page=#{location}"
+          response = Net::HTTP.get_response("wikitravel.org", url)
+          data = Hash.from_xml(response.body) unless response.nil? || response.body.nil?
+          
+          unless data.nil? || data["api"].nil? || data["api"]["parse"].nil?              
+              unless data["api"]["parse"]["sections"].nil?
+                  params[:location] = data["api"]["parse"]["title"]
+                  @lines = data["api"]["parse"]["sections"]["s"] unless data["api"]["parse"]["sections"]["s"].nil?   
+              end
+          end
+      end   
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WikiTravel::Application.config.secret_token = '3cd11915f0803887ca6dae046f89c37ba4706f08c095a8673420818665b506003d2a4a98305c62f8df15041919455609479a1f6abc35fce8f1e6c74b26c823b7'
